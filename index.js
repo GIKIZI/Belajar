04ee1dbfd438fce1a1f3b0c267edaaf3a0a5f234
brandnday <brandnday@gmail.com>
2019-05-12 17:45:38 +0700

    UMUR dan GENDER

---
+++ index.js
@@ -0,0 +1,14 @@
+if (umur <= 0) {
+  console.log("ngaco");
+} else if (umur <= 12) {
+  if (gender == "male") {
+    console.log("boys");
+  }else {
+    console.log("girl");
+  }
+  console.log("sm");
+} else if (umur >= 22) {
+  console.log("YOUTH");
+} else {
+  console.log("JC");
+}
