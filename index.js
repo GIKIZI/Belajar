89375a67e4842aacd118484eed4f4421cffdd86a
Gady <gradykenzei07@gmail.com>
2019-05-16 12:41:35 +0700

    newjs

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const gender="male";
-let umur=7;
+alert("tesssss");
 
+document.write("hahahaha");
 
-if (umur <= 0) {
-  console.log("ngaco");
-} else if (umur <= 12) {
-  if (gender == "male") {
-    console.log("boys");
-  }else {
-    console.log("girl");
-  }
-  console.log("sm");
-} else if (umur >= 22) {
-  console.log("YOUTH");
-} else {
-  console.log("JC");
-}
